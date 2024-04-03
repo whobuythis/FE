c57b59057f251bcd515dc61e85ef5ffacfb28513
ChanWoo Yun <yunchanwo@naver.com>
2024-04-03 14:02:28 +0900

    Update Login.vue
    finish

--- vue-firebase-tutorial/src/components/Login.vue
+++ vue-firebase-tutorial/src/components/Login.vue
@@ -1,23 +1,35 @@
 <template>
     <div class="login">
       <h3>Login</h3>
-      <input type="email" placeholder="email"><br>
-      <input type="password" placeholder="password"><br>
+      <input type="email" v-model="email" placeholder="email"><br>
+      <input type="password" v-model="password" placeholder="password"><br>
       <button v-on:click="login">로그인</button>
       <p>만약 계정이 없다면, <router-link to="/signup">회원가입</router-link>을 먼저 진행해주세요!</p>
     </div>
   </template>
   
   <script>
+  import firebase from 'firebase'
+  
     export default {
       name: 'login',
       data() {
         return {
+          email: '',
+          password: ''
         }
       },
       methods: {
         login() {
-          this.$router.replace('hello')
+          //this.$router.replace('hello')
+          firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+            function(user) {
+              alert('로그인 완료!')                         
+            },
+            function(err) {
+              alert('에러 : ' + err.message)
+            }
+          );
         }
       }
     }
